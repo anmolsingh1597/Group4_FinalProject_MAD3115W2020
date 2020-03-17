aabeea86de9789682dd2b5209b7b52db1115883e
Aman Kaur <amankaur2628@gmail.com>
2020-03-17 19:46:52 -0400

    [Aman]Person class: gender updated to enum

--- Group4_FinalProject_MAD3115W2020/Driver.swift
+++ Group4_FinalProject_MAD3115W2020/Driver.swift
@@ -13,7 +13,7 @@ class Driver: Person{
     var isDrivingHistoryCleared: String
     var salary: String
     
-    init(id: String, firstName: String, lastName: String, gender: String, birthDate: String, mobileNumber: String, emailId: String, userName: String, password: String, drivingLicenceNumber: String, isDrivingHistoryCleared:String, salary: String ) {
+    init(id: String, firstName: String, lastName: String, gender: Enum.Gender, birthDate: String, mobileNumber: String, emailId: String, userName: String, password: String, drivingLicenceNumber: String, isDrivingHistoryCleared:String, salary: String ) {
         self.drivingLicenceNumber = drivingLicenceNumber
         self.isDrivingHistoryCleared = isDrivingHistoryCleared
         self.salary = salary
