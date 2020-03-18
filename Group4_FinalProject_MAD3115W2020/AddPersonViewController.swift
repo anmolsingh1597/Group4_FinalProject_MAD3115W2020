025be6427fbdd5c4d1cd5123e982798706403a75
Aman Kaur <amankaur2628@gmail.com>
2020-03-18 01:37:17 -0400

    [Aman]PersonVC: Successfully designed

--- Group4_FinalProject_MAD3115W2020/AddPersonViewController.swift
+++ Group4_FinalProject_MAD3115W2020/AddPersonViewController.swift
@@ -9,13 +9,42 @@
 import UIKit
 
 class AddPersonViewController: UIViewController {
-
+    @IBOutlet weak var iGeneralTextField1: UITextField!
+    @IBOutlet weak var iGeneralTextField2: UITextField!
+    @IBOutlet weak var iGeneralTextField3: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+    }
+    @IBAction func iPersonSegment(_ sender: UISegmentedControl) {
+        personSegmentValues(index: sender.selectedSegmentIndex)
     }
     
+    private func personSegmentValues (index: Int){
+        switch index {
+        case 0:
+            iGeneralTextField1.placeholder = "Address"
+            iGeneralTextField2.placeholder = "City"
+            iGeneralTextField3.isHidden = true
+        case 1:
+            iGeneralTextField1.placeholder = "License Number"
+            iGeneralTextField2.placeholder = "Driving History"
+            iGeneralTextField3.isHidden = false
+            iGeneralTextField3.placeholder = "Salary"
+        case 2:
+            iGeneralTextField1.placeholder = "Company Title"
+            iGeneralTextField2.placeholder = "Business Number"
+            iGeneralTextField3.isHidden = false
+            iGeneralTextField3.placeholder = "Website"
+        default:
+            iGeneralTextField1.isHidden = true
+            iGeneralTextField2.isHidden = true
+            iGeneralTextField3.isHidden = false
+        }
+    }
 
     /*
     // MARK: - Navigation
