9e9fe291b644c7a02f640b6179fc7bc2dc15b2ab
Aman Kaur <amankaur2628@gmail.com>
2020-03-18 14:49:34 -0400

    [Aman]DataStorage: Functions declared

--- Group4_FinalProject_MAD3115W2020/DataStorage.swift
+++ Group4_FinalProject_MAD3115W2020/DataStorage.swift
@@ -20,5 +20,22 @@ class DataStorage{
     static func getInstance() -> DataStorage{
         return instance
     }
-    
+    func addCustomer(customer: Customer){
+        self.customerList.append(customer)
+    }
+    func addDriver(driver: Driver){
+        self.driverList.append(driver)
+    }
+    func addOwner(owner: Owner){
+        self.ownerList.append(owner)
+    }
+    func getAllCustomers() -> [Customer]{
+        return self.customerList
+    }
+    func getAllDrivers() -> [Driver]{
+        return self.driverList
+    }
+    func getAllOwners() -> [Owner]{
+        return self.ownerList
+    }
 }
