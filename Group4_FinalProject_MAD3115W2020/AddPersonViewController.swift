9a0b32d4dcf430e59a4837d90e3fa4ff77046080
Aman Kaur <amankaur2628@gmail.com>
2020-03-18 18:55:22 -0400

    [Aman]AddPersonVC: Data Store in real time

--- Group4_FinalProject_MAD3115W2020/AddPersonViewController.swift
+++ Group4_FinalProject_MAD3115W2020/AddPersonViewController.swift
@@ -73,24 +73,30 @@ class AddPersonViewController: UIViewController, UITextFieldDelegate{
         let email = self.iEmailId.text
         let userName = iUserName.text
         let password = iPassword.text
-
+        
          guard let key = self.ref.child("Persons").childByAutoId().key else {return}
         if iPersonSegmentValue.selectedSegmentIndex == 0{
             let address = iGeneralTextField1.text
             let city = iGeneralTextField2.text
-            self.insert = ["id": "CUS_" + (id ?? ""), "firstName": firstName ?? "", "lastName": lastName ?? "", "gender": gender ?? "", "birthDate": birthDate ?? "", "mobileNumber": mobileNumber ?? "", "email": email ?? "", "userName": userName ?? "","password": password ?? "", "address": address ?? "", "city": city ?? "", "personType": "Customer"]
+            self.insert = ["id": "CUS_" + (id ?? ""), "firstName": firstName ?? "", "lastName": lastName ?? "", "gender": gender ?? "", "birthDate": birthDate ?? "", "mobileNumber": mobileNumber ?? "", "email": email ?? "", "userName": userName ?? "","password": password ?? "", "address": address ?? "" , "city": city ?? "" , "personType": "Customer"]
+            
+            DataStorage.getInstance().addCustomer(customer: Customer.init(id: "CUS_" + (id ?? ""), firstName: firstName ?? "", lastName: lastName ?? "", gender: gender ?? "", birthDate: birthDate ?? "", mobileNumber: mobileNumber ?? "", emailId: email ?? "", userName: userName ?? "", password: password ?? "", address: address ?? "", city: city ?? ""))
         }
         else if iPersonSegmentValue.selectedSegmentIndex == 1{
             let drivingLicenseNumber = iGeneralTextField1.text
             let drivingHistory = iGeneralTextField2.text
             let salary = iGeneralTextField3.text
-            self.insert = ["id": "DRI_" + (id ?? ""), "firstName": firstName ?? "", "lastName": lastName ?? "", "gender": gender ?? "", "birthDate": birthDate ?? "", "mobileNumber": mobileNumber ?? "", "email": email ?? "", "userName": userName ?? "","password": password ?? "","drivingLicenseNumber": drivingLicenseNumber ?? "", "drivingHistory": drivingHistory ?? "","salary": salary ?? "", "personType": "Driver"]
+            self.insert = ["id": "DRI_" + (id ?? ""), "firstName": firstName ?? "", "lastName": lastName ?? "", "gender": gender ?? "", "birthDate": birthDate ?? "", "mobileNumber": mobileNumber ?? "", "email": email ?? "", "userName": userName ?? "","password": password ?? "","drivingLicenseNumber": drivingLicenseNumber ?? "" , "drivingHistory": drivingHistory ?? "" ,"salary": salary ?? "" , "personType": "Driver"]
+            
+            DataStorage.getInstance().addDriver(driver: Driver.init(id: "DRI_" + (id ?? ""), firstName: firstName ?? "", lastName: lastName ?? "", gender: gender ?? "", birthDate: birthDate ?? "", mobileNumber: mobileNumber ?? "", emailId: email ?? "", userName: userName ?? "", password: password ?? "", drivingLicenceNumber: drivingLicenseNumber ?? "", isDrivingHistoryCleared: drivingHistory ?? "", salary: salary ?? ""))
         }
         else if iPersonSegmentValue.selectedSegmentIndex == 2{
             let companyTitle = iGeneralTextField1.text
             let businessNumber = iGeneralTextField2.text
             let website = iGeneralTextField3.text
-            self.insert = ["id": "OWN_" + (id ?? ""), "firstName": firstName ?? "", "lastName": lastName ?? "", "gender": gender ?? "", "birthDate": birthDate ?? "", "mobileNumber": mobileNumber ?? "", "email": email ?? "", "userName": userName ?? "","password": password ?? "","companyTitle": companyTitle ?? "", "businessNumber": businessNumber ?? "", "website": website ?? "", "personType": "Owner"]
+            self.insert = ["id": "OWN_" + (id ?? ""), "firstName": firstName ?? "", "lastName": lastName ?? "", "gender": gender ?? "", "birthDate": birthDate ?? "", "mobileNumber": mobileNumber ?? "", "email": email ?? "", "userName": userName ?? "","password": password ?? "","companyTitle": companyTitle ?? <#default value#> , "businessNumber": businessNumber ?? <#default value#> , "website": website ?? <#default value#> , "personType": "Owner"]
+            
+            DataStorage.getInstance().addOwner(owner: Owner.init(id: "OWN_" + (id ?? ""), firstName: firstName ?? "", lastName: lastName ?? "", gender: gender ?? "", birthDate: birthDate ?? "", mobileNumber: mobileNumber ?? "", emailId: email ?? "", userName: userName ?? "", password: password ?? "", companyTitle: companyTitle ?? "", businessNumber: businessNumber ?? "", website: website ?? ""))
         }
         else {
             
@@ -105,7 +111,7 @@ class AddPersonViewController: UIViewController, UITextFieldDelegate{
                        }
         else{
             let childUpdates = ["/Persons/\(key)": insert]
-                              self.ref.updateChildValues(childUpdates)
+            self.ref.updateChildValues(childUpdates)
             let alertControll = UIAlertController(title: "Success!!", message: "User Added Successfully", preferredStyle: .alert)
             alertControll.addAction(UIAlertAction(title: "OK", style: .default))
             self.present(alertControll,animated: true,completion: nil)
