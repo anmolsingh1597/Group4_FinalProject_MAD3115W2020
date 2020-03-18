db2b8092928bef720edbef20631fa9427764210e
Aman Kaur <amankaur2628@gmail.com>
2020-03-18 13:44:30 -0400

    [Aman]DataStorage: class declared

---
+++ Group4_FinalProject_MAD3115W2020/DataStorage.swift
@@ -0,0 +1,9 @@
+//
+//  DataStorage.swift
+//  Group4_FinalProject_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-18.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
