b08153fcd5de348ae5a70b409e40a43f5f63897e
Aman Kaur <amankaur2628@gmail.com>
2020-03-19 14:27:58 -0400

    [Aman]NewVehicleVC: Generated

---
+++ Group4_FinalProject_MAD3115W2020/NewVehicleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewVehicleViewController.swift
+//  Group4_FinalProject_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-19.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class NewVehicleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
