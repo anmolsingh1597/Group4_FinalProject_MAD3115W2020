947e172aef76e8918372100a77f233c749b7e7a2
Aman Kaur <amankaur2628@gmail.com>
2020-03-19 15:31:27 -0400

    [Aman]Class Bus: init() declared

--- Group4_FinalProject_MAD3115W2020/Bus.swift
+++ Group4_FinalProject_MAD3115W2020/Bus.swift
@@ -13,6 +13,9 @@ class Bus: Vehicle{
     var isAccessibilityServiceavailable: String
     var isWifiAvailable: String
     init(vehicleIdentificationNumber: String, vehicleDescription: String, manufacturerName: String, isSelfDrive: String, driverName: String, isInsured: String, noOfSeat: String, fuelType: String, typeOfBus: String, isAccessibilityServiceavailable: String, isWifiAvailable:String){
-        
+        self.typeOfBus = typeOfBus
+        self.isAccessibilityServiceavailable = isAccessibilityServiceavailable
+        self.isWifiAvailable = isWifiAvailable
+        super.init(vehicleIdentificationNumber: vehicleIdentificationNumber, vehicleDescription: vehicleDescription, manufacturerName: manufacturerName, isSelfDrive: isSelfDrive, driverName: driverName, isInsured: isInsured, noOfSeat: noOfSeat, fuelType: fuelType)
     }
 }
