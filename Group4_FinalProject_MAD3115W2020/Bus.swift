cb9d160155beac1838ae70b63c2ad881994db47f
Aman Kaur <amankaur2628@gmail.com>
2020-03-19 15:28:59 -0400

    [Aman]Class Bus: Declared

---
+++ Group4_FinalProject_MAD3115W2020/Bus.swift
@@ -0,0 +1,15 @@
+//
+//  Bus.swift
+//  Group4_FinalProject_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-19.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+class Bus: Vehicle{
+    var typeOfBus: String
+    var isAccessibilityServiceavailable: String
+    var isWifiAvailable: String
+}
