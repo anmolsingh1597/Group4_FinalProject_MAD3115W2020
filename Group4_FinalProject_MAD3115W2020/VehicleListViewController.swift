dbb8a0dfa0798de4503e629dd04151b4f52552a9
Aman Kaur <amankaur2628@gmail.com>
2020-03-19 14:34:51 -0400

    [Aman]VehicleListVC: Table Protocol extended

--- Group4_FinalProject_MAD3115W2020/VehicleListViewController.swift
+++ Group4_FinalProject_MAD3115W2020/VehicleListViewController.swift
@@ -28,3 +28,16 @@ class VehicleListViewController: UIViewController {
     */
 
 }
+
+extension VehicleListViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        return UITableViewCell
+    }
+    
+    
+}
