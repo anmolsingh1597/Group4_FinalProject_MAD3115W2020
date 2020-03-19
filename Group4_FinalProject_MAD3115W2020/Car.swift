490e148c07f88096aaa70e72c3657bab588296ea
Aman Kaur <amankaur2628@gmail.com>
2020-03-19 15:21:36 -0400

    [Aman]Class Car: Declared

---
+++ Group4_FinalProject_MAD3115W2020/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  Group4_FinalProject_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-19.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+class Car{
+    
+}
