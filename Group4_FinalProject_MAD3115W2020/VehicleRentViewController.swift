f096ef5d9eb130829660107bc41df38aa021f7f4
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-24 08:45:34 -0400

    [Anmol Singh]VehicleRentVC: Designed

---
+++ Group4_FinalProject_MAD3115W2020/VehicleRentViewController.swift
@@ -0,0 +1,34 @@
+//
+//  VehicleRentViewController.swift
+//  Group4_FinalProject_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-24.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class VehicleRentViewController: UIViewController {
+    @IBOutlet weak var iId: UILabel!
+    @IBOutlet weak var iFirstName: UILabel!
+    static var id = String()
+    static var firstName = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
