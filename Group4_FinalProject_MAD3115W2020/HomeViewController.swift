d8ba8483163edf3944770cd16bd45d7363028208
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-26 18:27:35 -0400

    [Anmol Singh]LoginVC: Remember Me working properly

--- Group4_FinalProject_MAD3115W2020/HomeViewController.swift
+++ Group4_FinalProject_MAD3115W2020/HomeViewController.swift
@@ -38,7 +38,6 @@ self.navigationItem.setHidesBackButton(true, animated: true);
                        }
     }
     @IBAction func iLogin(_ sender: UIButton) {
-                print("login")
                         let user = self.iLoginId.text
                         let password = self.iPassword.text
                 
@@ -67,49 +66,6 @@ self.navigationItem.setHidesBackButton(true, animated: true);
     }
     
 
-//    @IBAction func iLogin(_ sender: UIBarButtonItem) {
-//        print("login")
-//                let user = self.iLoginId.text
-//                let password = self.iPassword.text
-//
-////                 To input data in Firebase
-//
-//                guard let key = self.ref.child("Logins").childByAutoId().key else { return }
-//                let insert = ["userName": user,"password": password]
-////                let refer = self.ref.child("Logins")
-////                refer.observeSingleEvent(of: .value, with: { (snapshot) in
-////                if let userDict = snapshot.value as? [String:[String:String]]{
-////                if userDict.values.contains(insert as! [String: String]){
-////                print("User exist")
-////                }else{
-//        // values inserted successfully
-//                let childUpdates = ["/Logins/\(key)": insert]
-//                self.ref.updateChildValues(childUpdates)
-////                }
-//            }
-////        })
-//        //-----------------------------------------
-//
-////                let refer = self.ref.child("Logins")
-////                refer.observeSingleEvent(of: .value, with: { (snapshot) in
-////                if let userDict = snapshot.value as? [String:[String:String]] {
-////                   // print(userDict.values)
-////                let loginData = ["userName": user, "password": password]
-////                if userDict.values.contains(loginData as! [String : String]){
-////                    //print("It worked")
-////                    let sb = UIStoryboard(name: "Main", bundle: nil)
-////                    let secondVC = sb.instantiateViewController(identifier: "listsVC") as! ViewController
-////                    self.navigationController?.pushViewController(secondVC, animated: true)
-////                }
-////                else{
-////                   let alertControll = UIAlertController(title: "Log In Failed", message: "Invalid User-name and Password ", preferredStyle: .alert)
-////                    alertControll.addAction(UIAlertAction(title: "Ok", style: .default))
-////                    self.present(alertControll, animated: true, completion: nil)
-////                    }
-////        }
-////                }
-////                )
-   
     
         @objc func stateChanged(_ switchState: UISwitch) {
 
