e8c70f73affddce6179e6f5f9251d9d82400807d
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-26 20:57:11 -0400

    [Anmol Singh]Vehicle Rent Class: Generated

---
+++ Group4_FinalProject_MAD3115W2020/Vehicle_Rent/VehicleRent.swift
@@ -0,0 +1,19 @@
+//
+//  VehicleRent.swift
+//  Group4_FinalProject_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-26.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import Foundation
+
+struct VehicleRent{
+    var custId: String
+    var name: String
+    var vin: String
+    var vehicle: String
+    var noOfDays: String
+    var noOfKmDrived: String
+    var totalFare: String
+}
